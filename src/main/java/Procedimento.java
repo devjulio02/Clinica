7132aa3b828da3e440f6372f737828a8ab6a108d
Julio <soares.julio@academico.ifpb.edu.br>
2026-05-09 18:43:23 -0300

    Subindo diretório pela primeira vez

---
+++ src/main/java/Procedimento.java
@@ -0,0 +1,14 @@
+public class Procedimento {
+
+	private TipoProcedimento tipoProcedimento;
+
+	public Procedimento(TipoProcedimento tipoProcedimento) {
+		this.tipoProcedimento = tipoProcedimento;
+	}
+
+	public TipoProcedimento getTipoProcedimento() {
+		return this.tipoProcedimento;
+	}
+
+}
+
